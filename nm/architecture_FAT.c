9e63144a80dee3685ac3bbeaaefadfb99f1807eb
Thomas <thomas.pro@tutanota.com>
2020-01-25 18:45:59 +0100

    Modification FAT :dog2:

--- nm/architecture_FAT.c
+++ nm/architecture_FAT.c
@@ -19,18 +19,24 @@
 
 int				ft_handle_FAT_architecture(struct s_file *file)
 {
+	uint32_t		magic;
 	struct fat_header	header;
 	struct fat_arch		architectures;
 	struct fat_arch		*offset;
 
 	memcpy(&header, (void *)file->content, sizeof(struct fat_header));
+	magic = header.magic;
 	if (header.magic == FAT_CIGAM || header.magic == FAT_CIGAM_64)
 		swap_fat_header(&header, NXHostByteOrder());
 	offset = (struct fat_arch *)((uintptr_t)file->content + sizeof(struct fat_header));
 	for (unsigned int index = 0; index < header.nfat_arch; ++index) {
 		memcpy(&architectures, offset++, sizeof(struct fat_arch));
-		swap_fat_arch(&architectures, header.nfat_arch, NXHostByteOrder());
-		ft_parse_mach_section(file, (void *)((uintptr_t)file->content + architectures.offset));
+		if (magic == FAT_CIGAM || magic == FAT_CIGAM_64)
+			swap_fat_arch(&architectures, header.nfat_arch, NXHostByteOrder());
+		if (architectures.cputype == CPU_TYPE_X86_64) {
+			ft_parse_mach_section(file, (void *)((uintptr_t)file->content + architectures.offset));
+			break ;
+		}
 	}
 	return (EXIT_SUCCESS);
 }
