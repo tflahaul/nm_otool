707258957a19e015def839f4ba3d7709d6d52184
tflahaul <thomas.pro@tutanota.com>
2020-12-27 14:45:19 +0100

    segv in otool

--- src/otool/main.c
+++ src/otool/main.c
@@ -6,7 +6,7 @@
 /*   By: thflahau <thflahau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/23 17:30:56 by thflahau          #+#    #+#             */
-/*   Updated: 2020/11/26 15:12:35 by thflahau         ###   ########.fr       */
+/*   Updated: 2020/12/27 13:05:52 by thflahau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ int				main(int const argc, char const **argv)
 	struct arguments	args;
 
 	parse_arguments(&args, argc, argv);
-	for (unsigned int index = 0; index < args.size; ++index) {
-		if (load_file_informations(&file, args.arguments[index]) == EXIT_SUCCESS) {
+	for (args.idx = 0; args.idx < args.size; ++args.idx) {
+		if (load_file_informations(&file, args.arguments[args.idx]) == EXIT_SUCCESS) {
 			print_section(&file, &args);
 			HANDLE_GNU_ERROR(munmap(file.head, file.length), strerror(errno));
 		}
